f48fb735b13b58ae07591a6cebffed73494887dc
risa <az.5s.fmt@gmail.com>
2019-12-15 17:54:58 +0900

    タスクの追加と完了

--- store.js
+++ store.js
@@ -17,6 +17,30 @@ const store = new Vuex.Store({
                 done: true
             }
         ],
+
+        nextTaskId: 3,
+    },
+
+    mutations: {
+        addTask (state, {name}) {
+            state.tasks.push({
+                id: state.nextTaskId,
+                name,
+                done: false
+            })
+
+            state.nextTaskId++
+        },
+
+        toggleTaskStatus (state, {id}) {
+            const filtered = state.tasks.filter(task => {
+                return task.id === id
+            })
+
+            filtered.forEach(task => {
+                task.done = !task.done
+            })
+        },
     },
 })
 
